cd73c1c7b521c6f0ba324a1b3f2266d6a5e88d05
TheMachineDK123 <machinedk123@gmail.com>
2026-02-14 09:34:51 +0100

    Update config.lua

--- TheMach1neDK_Helicam/config.lua
+++ TheMach1neDK_Helicam/config.lua
@@ -1,18 +1,18 @@
 Config = {}
 
 Config.Locale = 'da'
-
+//Job-Whitelist//
 Config.AllowedJobs = {
     police = true,
 }
-
+//Helikoptere//
 Config.AllowedHelis = {
     polmav = true,
     maverick = true,
 }
-
+//Skal Helicam være låst på specifikke sæder?//
 Config.RequireSeat = false
-
+//Keybinds//
 Config.ToggleKey = 74
 Config.LockKey = 38
 Config.SpotlightKey = 47
@@ -21,14 +21,14 @@ Config.NightVisionKey = 249
 Config.ThermalKey = 182
 Config.CopyPlateKey = 311
 Config.ExitKey = 177
-
+//Zoom indstillinger//
 Config.Zoom = {
     MinFov = 15.0,
     MaxFov = 65.0,
     Step = 2.5,
     Smooth = 8.0,
 }
-
+//Kamera settings//
 Config.Look = {
     SpeedKeyboard = 3.0,
     SpeedMouse = 7.5,
@@ -41,7 +41,7 @@ Config.Targeting = {
     LockBreakDistance = 1200.0,
     RaycastFlags = 10,
 }
-
+//Lås på køretøjer//
 Config.Spotlight = {
     CustomEnabled = true,
     Distance = 150.0,
@@ -52,18 +52,18 @@ Config.Spotlight = {
     Color = { r = 235, g = 255, b = 255 },
     Offset = { x = 0.0, y = 2.0, z = -1.2 },
 }
-
+//Nummerpladescanner settings//
 Config.ANPR = {
     Enabled = true,
     OnlyWhenLocked = true,
     IntervalMs = 2500,
     CooldownMs = 8000,
 }
-
+//Database log//
 Config.Database = {
     LogANPR = false,
 }
-
+//Køretøjs ejerskab DB settings//
 Config.Ownership = {
     Enabled = true,
     OwnedVehiclesTable = 'owned_vehicles',
